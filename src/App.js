056d15abb25fa6d619104af98fe7f9243aa80716
Janvinsha <janvinsha@gmail.com>
2022-03-13 18:53:09 +0100

    Edit event listener

--- src/App.js
+++ src/App.js
@@ -172,6 +172,7 @@ function App() {
     });
     connectedContract.on('SetName', name => {
       fetchAccountDetails();
+      checkIfWalletIsConnected();
       console.log('SET NAME TO', name);
     });
     // Same stuff again
